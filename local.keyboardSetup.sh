8ff14bf860e193142be3e9f2e86a6b35c954d4ee
Péter Szabó <szabo.m.peter@gmail.com>
2024-05-07 21:22:38 +0200

    Initial version

---
+++ local.keyboardSetup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+hidutil property --set \
+    '{
+       "UserKeyMapping":[
+         {
+             "HIDKeyboardModifierMappingSrc":0x700000035,
+             "HIDKeyboardModifierMappingDst":0x700000064
+         }
+       ]
+     }'
