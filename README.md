e1fac5f3924d9734a12121f96e5cf7d1944a5b89
Péter Szabó <szabo.m.peter@gmail.com>
2024-05-07 22:29:06 +0200

    First (untested) version

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # macos_keyboard_setup
+
+Contains changes to MacOS keyboard layout.
+
+Unfortonatel MacOS does some low level magic arond the `~` key in ISO/ANSI mixed keyboard setups.
+
+This startup script changes keyboard button `§±` to `international backslash`.
+
+# Install
+
+Run the script `install.sh`.
+
+It will 
+ - create a `plist` file for `launchd`,
+ - link it into `/Library/LaunchDaemons`
+
+
--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
-SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+#!/bin/sh
 
-echo $SCRIPTPATH
+INSTALL_SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+SCRIPT_PATH="$INSTALL_SCRIPT_DIR/local.keyboardSetup.sh"
+TARGET_PLIST=/Library/LaunchDaemons/local.keyboardSetup.plist 
+
+SCRIPT_PATH_PATTERN=`echo "$SCRIPT_PATH" | sed 's_/_\\\\/_g'`
+
+sed "s/##SCRIPT_LOCATION##/$SCRIPT_PATH_PATTERN/g" < template.local.keyboardSetup.plist > local.keyboardSetup.plist
+
+sudo chmod +x "$SCRIPT_PATH"
+
+if [ -e "$TARGET_PLIST" ]; then
+    sudo rm "$TARGET_PLIST";
+fi
+sudo ln -s "$INSTALL_SCRIPT_DIR/local.keyboardSetup.plist" "$TARGET_PLIST"
